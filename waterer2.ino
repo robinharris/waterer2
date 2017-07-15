b7409b694f74a3d2c658ab4630da550b1614f3b2
Robin Harris <rh@c4di.net>
2017-07-15 07:09:10 +0100

    Test for new FETs

--- waterer2.ino
+++ waterer2.ino
@@ -22,8 +22,8 @@ char wifiPassword[] = "E0E3106433F4";
 
 const char* mqtt_server = "192.168.0.36";
 char msg[100];
-const char *topicToPublish = "Chillis"; //mqtt topic for moisture
-const char *pumpTopic = "Pump"; //mqtt topic for pump status
+const char *topicToPublish = "Chillis2"; //mqtt topic for moisture
+const char *pumpTopic = "Pump_2"; //mqtt topic for pump status
 char *pumpStatus = "";
 
 
@@ -34,10 +34,10 @@ PubSubClient client(espClient);
 // PUMP VARIABLES - INTERVAL AND DURATION
 //number of mS between pump running 86400000 is 1 day
 const unsigned long timeBetweenWatering = 60 * 60 * 1000;
-unsigned long pumpRunDuration = 10000;
+unsigned long pumpRunDuration = 15000;
 // ======================================================================
 
-unsigned long currentMillis = 60 * 60 * 1000;
+unsigned long currentMillis = timeBetweenWatering;
 unsigned long previousMillis = 0;
 unsigned long pumpStartMillis;
 
@@ -54,8 +54,8 @@ void callback(char* topic, byte* payload, unsigned int length);
 
 
 void setup(){
-// Serial.begin(9600);
-// Serial.println("Starting.....");
+Serial.begin(9600);
+Serial.println("Starting.....");
 pinMode(pump1Pin, OUTPUT);
 pinMode(pump2Pin, OUTPUT);
 pinMode(sensorVoltagePin, OUTPUT);
@@ -71,8 +71,8 @@ WiFi.begin(ssid, wifiPassword);
     // Serial.print(".");
     delay(200);
   }
-// Serial.print("Connected, IP = ");
-// Serial.println(WiFi.localIP());
+Serial.print("Connected, IP = ");
+Serial.println(WiFi.localIP());
 
 client.setServer(mqtt_server, 1883);
 client.setCallback(callback);
@@ -88,9 +88,9 @@ currentMillis = millis();
 //check if it is time to add water
 if (currentMillis - previousMillis >= timeBetweenWatering){
 	// first check if there is water in the tray
-  if (getMoisture() > 500){
+  if (getMoisture() > 0){
     //run pump1 for the set period
-    // Serial.print("Pump1 started\t");
+    Serial.print("Pump1 started\t");
     pumpStatus = "Started pump 1";
     publishPump(pumpTopic, pumpStatus);
     digitalWrite(pump1Pin, HIGH);
@@ -99,10 +99,10 @@ if (currentMillis - previousMillis >= timeBetweenWatering){
     digitalWrite(pump1Pin, LOW);
     pumpStatus = "Stopped pump 1";
     publishPump(pumpTopic, pumpStatus);
-    // Serial.println("Pump1 stopped");
+    Serial.println("Pump1 stopped");
     //run pump2 for the set period
     pumpStartMillis = millis();
-    // Serial.print("Pump2 started\t");
+    Serial.print("Pump2 started\t");
     pumpStatus = "Started pump 2";
     publishPump(pumpTopic, pumpStatus);
     digitalWrite(pump2Pin, HIGH);
@@ -111,7 +111,7 @@ if (currentMillis - previousMillis >= timeBetweenWatering){
     digitalWrite(pump2Pin, LOW);
     pumpStatus = "Stopped pump 2";
     publishPump(pumpTopic, pumpStatus);
-    // Serial.println("Pump2 stopped");
+    Serial.println("Pump2 stopped");
   }//end of adding water block if moisure reading is low enough
   previousMillis = currentMillis;
 }//end of block if it is time to water
@@ -148,7 +148,7 @@ int getMoisture() {
   int cumulativeLevel = 0;
   for (int i; i<10; i++){
     cumulativeLevel = cumulativeLevel + analogRead(A0);
-    delay(500);
+    delay(100);
   }
   //turn off voltage to moisture sensor
   digitalWrite(sensorVoltagePin, LOW);
@@ -163,8 +163,8 @@ void reconnect() {
   while (!client.connected()) {
     // Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("Chilli_Waterer")) {
-      // Serial.println("connected");
+    if (client.connect("Chilli_Waterer2")) {
+      Serial.println("connected");
       // Once connected, publish an announcement...
       // client.publish("outTopic", "hello world");
       // ... and resubscribe
@@ -176,18 +176,19 @@ void reconnect() {
       // Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
-    }// end else
-  }// end if
+    }// end if
+    delay(1000);
+  }// end while
 }// end reconnect
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  // Serial.print("Message arrived [");
-  // Serial.print(topic);
-  // Serial.print("] ");
-  // for (int i = 0; i < length; i++) {
-  //   Serial.print((char)payload[i]);
-  // }
-  // Serial.println();
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
 
   // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1') {
